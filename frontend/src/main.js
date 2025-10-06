59fa6bcd3e6415a8bcdecad8b01ed4bf47a2928e
1VicTim1 <122aaa121@gmail.com>
2025-10-07 00:05:30 +0500

    asd

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import {loadTheme} from './stores/theme'
+import {loadTheme} from './stores/theme.ts'
 
 const app = createApp(App)
 
